20be187fe91ef2085685f8bfc9691af2f4862f3e
Ravi Kajan <61968423+ravikajan@users.noreply.github.com>
2024-03-27 19:21:34 +0530

    create user props added

--- src/action.ts/user.ts
+++ src/action.ts/user.ts
@@ -4,8 +4,8 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 
-const CreateUser = async () => {
-    const user = await prisma.user.create({ data: { username: "kajan", password: "password", email: "kajan@prisma.com" } })
+const CreateUser = async ({ email, username, password }:any) => {
+    const user = await prisma.user.create({ data: { email, username, password } })
     console.log(user)
     return user
 }
